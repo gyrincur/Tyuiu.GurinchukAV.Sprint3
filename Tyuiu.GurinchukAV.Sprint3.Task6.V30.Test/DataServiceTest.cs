c4d6cda10513ce7792682aa1943affc32455cb72
Анастасия Гуринчук <gurincuknasta59@gmail.com>
2025-10-28 21:02:21 +0300

    Task6

---
+++ Tyuiu.GurinchukAV.Sprint3.Task6.V30.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.GurinchukAV.Sprint3.Task6.V30.Lib;
+namespace Tyuiu.GurinchukAV.Sprint3.Task6.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 151; 
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GurinchukAV.Sprint3.Task6.V30/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.GurinchukAV.Sprint3.Task6.V30.Lib;
+namespace Tyuiu.GurinchukAV.Sprint3.Task6.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            Console.WriteLine($"Начало диапазона = {startValue}");
+            Console.WriteLine($"Конец диапазона = {stopValue}");
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int result = ds.GetSumTheDivisors(startValue, stopValue);
+
+            Console.WriteLine($"Сумма всех делителей = {result}");
+            Console.ReadKey();
+        }
+    }
+}
