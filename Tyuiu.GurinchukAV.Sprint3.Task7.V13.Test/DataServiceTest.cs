d86cbde34945f52275924652ed0edb4da6f39e59
Анастасия Гуринчук <gurincuknasta59@gmail.com>
2025-10-28 22:25:11 +0300

    Task7

---
+++ Tyuiu.GurinchukAV.Sprint3.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.GurinchukAV.Sprint3.Task7.V13.Lib;
+namespace Tyuiu.GurinchukAV.Sprint3.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = -9.1;
+            valueWaitArray[1] = 1.55;
+            valueWaitArray[2] = 292.78;
+            valueWaitArray[3] = -0.57;
+            valueWaitArray[4] = -0.35;
+            valueWaitArray[5] = 2;
+            valueWaitArray[6] = 4.35;
+            valueWaitArray[7] = 4.57;
+            valueWaitArray[8] = -288.78;
+            valueWaitArray[9] = 2.45;
+            valueWaitArray[10] = 13.1;
+
+            double[] res;
+
+            res = ds.GetMassFunction(startValue, stopValue);
+
+
+            Console.WriteLine("Expected: " + string.Join(",", valueWaitArray));
+            Console.WriteLine("Actual: " + string.Join(", ", res));
+
+
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.GurinchukAV.Sprint3.Task7.V13/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.GurinchukAV.Sprint3.Task7.V13.Lib;
+namespace Tyuiu.GurinchukAV.Sprint3.Task7.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine($"Начало диапазона = {startValue}");
+            Console.WriteLine($"Конец диапазона = {stopValue}");
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|    X     |    f(x)   |");
+            Console.WriteLine("+----------+-----------+");
+
+            int x = startValue;
+            for (int i = 0; i < res.Length; i++, x++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1,6:f2}   |", x, res[i]);
+            }
+
+            Console.WriteLine("+----------+-----------+");
+            Console.ReadKey();
+        }
+    }
+}
